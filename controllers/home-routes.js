9f0d028a88a3e1d10b099d0cd5c0b7ef8b4b4100
Lucile <developer.lvt@gmail.com>
2021-05-30 20:00:25 -0400

    added sequelize query to get all posts on dashboard

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,24 +1,30 @@
 const router = require("express").Router();
+const sequelize = require("../config/connection");
+const { Post } = require("../models");
 
-router.get('/', (req,res)=>{
-    res.render('homepage')
+router.get("/", (req, res) => {
+  res.render("homepage");
 });
 
-router.get('/login', (req, res) => {
-    res.render('login');
+router.get("/login", (req, res) => {
+  res.render("login");
 });
 
-router.get('/signup', (req, res) => {
-  res.render('signup');
+router.get("/signup", (req, res) => {
+  res.render("signup");
 });
 
-router.get('/dashboard', (req, res) => {
-  res.render('dashboard');
+router.get("/dashboard", (req, res) => {
+  
+  Post.findAll({}).then((dbPostData) => {
+    const posts = dbPostData.map(post => post.get({ plain: true }));
+    res.render("dashboard",{posts});
+  });
+  
 });
 
-router.get('/dashboard/new', (req, res) => {
-  res.render('newpost');
+router.get("/dashboard/new", (req, res) => {
+  res.render("newpost");
 });
-
 
 module.exports = router;
