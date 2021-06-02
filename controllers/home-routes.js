c2917d50bd7af47ddb48af7661e2407f2ac878e6
Lucile <developer.lvt@gmail.com>
2021-06-02 16:23:08 -0400

    added loggedIn session variable to front routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,30 +4,29 @@ const { Post } = require("../models");
 
 router.get("/", (req, res) => {
   Post.findAll({}).then((dbPostData) => {
-    const posts = dbPostData.map(post => post.get({ plain: true }));
-    res.render("homepage",{posts});
+    const posts = dbPostData.map((post) => post.get({ plain: true }));
+    res.render("homepage", { posts,  loggedIn: req.session.loggedIn });  
+    });
+    
   });
-});
 
 router.get("/login", (req, res) => {
-  res.render("login");
+  res.render("login",{ loggedIn: req.session.loggedIn});
 });
 
 router.get("/signup", (req, res) => {
-  res.render("signup");
+  res.render("signup", { loggedIn: req.session.loggedIn});
 });
 
 router.get("/dashboard", (req, res) => {
-  
   Post.findAll({}).then((dbPostData) => {
-    const posts = dbPostData.map(post => post.get({ plain: true }));
-    res.render("dashboard",{posts});
+    const posts = dbPostData.map((post) => post.get({ plain: true }));
+    res.render("dashboard", { posts, loggedIn: req.session.loggedIn });
   });
-  
 });
 
 router.get("/dashboard/new", (req, res) => {
-  res.render("newpost");
+  res.render("newpost", { loggedIn: req.session.loggedIn});
 });
 
 module.exports = router;
