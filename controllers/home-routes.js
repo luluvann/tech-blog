78a27d66192b66d177479022ee7dcde0daac6806
Lucile <developer.lvt@gmail.com>
2021-06-02 16:57:39 -0400

    added home routes for posts

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -5,17 +5,16 @@ const { Post } = require("../models");
 router.get("/", (req, res) => {
   Post.findAll({}).then((dbPostData) => {
     const posts = dbPostData.map((post) => post.get({ plain: true }));
-    res.render("homepage", { posts,  loggedIn: req.session.loggedIn });  
-    });
-    
+    res.render("homepage", { posts, loggedIn: req.session.loggedIn });
   });
+});
 
 router.get("/login", (req, res) => {
-  res.render("login",{ loggedIn: req.session.loggedIn});
+  res.render("login", { loggedIn: req.session.loggedIn });
 });
 
 router.get("/signup", (req, res) => {
-  res.render("signup", { loggedIn: req.session.loggedIn});
+  res.render("signup", { loggedIn: req.session.loggedIn });
 });
 
 router.get("/dashboard", (req, res) => {
@@ -26,7 +25,26 @@ router.get("/dashboard", (req, res) => {
 });
 
 router.get("/dashboard/new", (req, res) => {
-  res.render("newpost", { loggedIn: req.session.loggedIn});
+  res.render("newpost", { loggedIn: req.session.loggedIn });
+});
+
+router.get("/posts/:id", (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbPostsData) => {
+      if (!dbPostsData) {
+        res.status(404).json({ message: "No post found with this id" });
+        return;
+      }
+      const post = dbPostsData.get({ plain: true });
+      res.render("post", { post, loggedIn: req.session.loggedIn });
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
